896fa20cfbd6a99f54aedfeac0f355647e851331
Rodrigo Dirk <rodrigodirk@nasajon.com.br>
2014-10-09 11:08:55 -0300

    Modificado para fazer como que o LogoutReceiveRequest faça redirect ao invés de retornar um XML.

--- src/AerialShip/SamlSPBundle/Bridge/LogoutReceiveRequest.php
+++ src/AerialShip/SamlSPBundle/Bridge/LogoutReceiveRequest.php
@@ -2,6 +2,9 @@
 
 namespace AerialShip\SamlSPBundle\Bridge;
 
+use AerialShip\LightSaml\Binding\BindingDetector;
+use AerialShip\LightSaml\Binding\HttpRedirect;
+use AerialShip\LightSaml\Binding\PostResponse;
 use AerialShip\LightSaml\Helper;
 use AerialShip\LightSaml\Meta\SerializationContext;
 use AerialShip\LightSaml\Model\Metadata\KeyDescriptor;
@@ -15,39 +18,39 @@ use AerialShip\SamlSPBundle\Config\ServiceInfo;
 use AerialShip\SamlSPBundle\Config\ServiceInfoCollection;
 use AerialShip\SamlSPBundle\RelyingParty\RelyingPartyInterface;
 use AerialShip\SamlSPBundle\State\SSO\SSOStateStoreInterface;
+use InvalidArgumentException;
+use RuntimeException;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\SecurityContextInterface;
 use Symfony\Component\Security\Http\HttpUtils;
 
+class LogoutReceiveRequest extends LogoutBase implements RelyingPartyInterface {
 
-class LogoutReceiveRequest extends LogoutBase implements RelyingPartyInterface
-{
     /** @var BindingManager */
     protected $bindingManager;
 
     /** @var ServiceInfoCollection  */
     protected $serviceInfoCollection;
 
-
+    /** @var SecurityContextInterface */
+    protected $securityContext;
 
     public function __construct(
-            BindingManager $bindingManager,
-            SSOStateStoreInterface $ssoStore,
-            ServiceInfoCollection $serviceInfoCollection,
-            HttpUtils $httpUtils
+    BindingManager $bindingManager, SSOStateStoreInterface $ssoStore, ServiceInfoCollection $serviceInfoCollection, HttpUtils $httpUtils, SecurityContextInterface $securityContext
     ) {
         parent::__construct($ssoStore, $httpUtils);
         $this->bindingManager = $bindingManager;
         $this->serviceInfoCollection = $serviceInfoCollection;
+        $this->securityContext = $securityContext;
     }
 
-
     /**
-     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param Request $request
      * @return bool
      */
-    public function supports(Request $request)
-    {
+    public function supports(Request $request) {
         if ($request->attributes->get('logout_path') != $request->getPathInfo()) {
             return false;
         }
@@ -58,15 +61,14 @@ class LogoutReceiveRequest extends LogoutBase implements RelyingPartyInterface
     }
 
     /**
-     * @param \Symfony\Component\HttpFoundation\Request $request
-     * @throws \RuntimeException
-     * @throws \InvalidArgumentException if cannot manage the Request
-     * @return \Symfony\Component\HttpFoundation\Response|SamlSpInfo|null
+     * @param Request $request
+     * @throws RuntimeException
+     * @throws InvalidArgumentException if cannot manage the Request
+     * @return Response|SamlSpInfo|null
      */
-    public function manage(Request $request)
-    {
+    public function manage(Request $request) {
         if (!$this->supports($request)) {
-            throw new \InvalidArgumentException('Unsupported request');
+            throw new InvalidArgumentException('Unsupported request');
         }
 
         $logoutRequest = $this->receiveRequest($request);
@@ -79,6 +81,7 @@ class LogoutReceiveRequest extends LogoutBase implements RelyingPartyInterface
         $logoutResponse->setID(Helper::generateID());
         $logoutResponse->setIssuer($serviceInfo->getSpProvider()->getEntityDescriptor()->getEntityID());
         $logoutResponse->setInResponseTo($logoutRequest->getID());
+        $logoutResponse->setRelayState($logoutRequest->getRelayState());
 
         $arrSLO = $serviceInfo->getIdpProvider()->getEntityDescriptor()->getFirstIdpSsoDescriptor()->findSingleLogoutServices();
         /** @var  $slo SingleLogoutService */
@@ -91,61 +94,77 @@ class LogoutReceiveRequest extends LogoutBase implements RelyingPartyInterface
 
         $context = new SerializationContext();
         $logoutResponse->getXml($context->getDocument(), $context);
+
+        // Log the user out
+        $request->getSession()->invalidate();
+        $this->securityContext->setToken(null);
+
+        // Return the response to SLO Service
+        $bindingType = $serviceInfo->getSpMetaProvider()->getSpMeta()->getResponseBinding();
+        if ($bindingType) {
+            $detector = new BindingDetector();
+            $binding = $detector->instantiate($bindingType);
+        } else {
+            $binding = new HttpRedirect();
+        }
+        $bindingResponse = $binding->send($logoutResponse);
+
+        if ($bindingResponse instanceof PostResponse) {
+            return new Response($bindingResponse->render());
+        } else if ($bindingResponse instanceof RedirectResponse) {
+            return new RedirectResponse($bindingResponse->getDestination());
+        }
+
         $xml = $context->getDocument()->saveXML();
 
         return new Response($xml, 200, array('Content-Type' => 'application/xml'));
     }
 
-
     /**
      * @param Request $request
      * @return LogoutRequest
-     * @throws \InvalidArgumentException
+     * @throws InvalidArgumentException
      */
-    protected function receiveRequest(Request $request)
-    {
+    protected function receiveRequest(Request $request) {
         /** @var  $logoutRequest LogoutRequest */
         $logoutRequest = $this->bindingManager->receive($request);
         if (!$logoutRequest || !$logoutRequest instanceof LogoutRequest) {
-            throw new \InvalidArgumentException('Did not receive logout request');
+            throw new InvalidArgumentException('Did not receive logout request');
         }
 
         return $logoutRequest;
     }
 
-
     /**
      * @param LogoutRequest $logoutRequest
      * @return ServiceInfo|null
-     * @throws \RuntimeException
+     * @throws RuntimeException
      */
-    protected function getServiceInfo(LogoutRequest $logoutRequest)
-    {
+    protected function getServiceInfo(LogoutRequest $logoutRequest) {
         $serviceInfo = $this->serviceInfoCollection->findByIDPEntityID($logoutRequest->getIssuer());
         if (!$serviceInfo) {
-            throw new \RuntimeException('Got logout request from unknown IDP: '.$logoutRequest->getIssuer());
+            throw new RuntimeException('Got logout request from unknown IDP: ' . $logoutRequest->getIssuer());
         }
 
         return $serviceInfo;
     }
 
     /**
-     * @param \AerialShip\SamlSPBundle\Config\ServiceInfo $serviceInfo
+     * @param ServiceInfo $serviceInfo
      * @param LogoutRequest $logoutRequest
-     * @throws \RuntimeException
+     * @throws RuntimeException
      */
-    protected function validateLogoutRequest(ServiceInfo $serviceInfo, LogoutRequest $logoutRequest)
-    {
+    protected function validateLogoutRequest(ServiceInfo $serviceInfo, LogoutRequest $logoutRequest) {
         $idp = $serviceInfo->getIdpProvider()->getEntityDescriptor();
         $keyDescriptors = $idp->getFirstIdpSsoDescriptor()->getKeyDescriptors();
         if (empty($keyDescriptors)) {
-            throw new \RuntimeException('IDP must support signing for logout requests');
+            throw new RuntimeException('IDP must support signing for logout requests');
         }
 
         /** @var  $signature SignatureValidatorInterface */
         $signature = $logoutRequest->getSignature();
         if (!$signature) {
-            throw new \RuntimeException('Logout request must be signed');
+            //  throw new RuntimeException('Logout request must be signed');
         }
 
         $keys = array();
@@ -154,9 +173,7 @@ class LogoutReceiveRequest extends LogoutBase implements RelyingPartyInterface
             $keys[] = $key;
         }
 
-        $signature->validateMulti($keys);
+        //$signature->validateMulti($keys);
     }
 
-
-
-} 
+}
